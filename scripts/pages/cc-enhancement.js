c7d18da516aafb5a06586ed051dabad720f4ff2f
matt <diggz50@gmail.com>
2018-11-29 22:33:05 -0600

    new HTML

--- scripts/pages/cc-enhancement.js
+++ scripts/pages/cc-enhancement.js
@@ -1,1 +1,1 @@
-var ccEnchancement=function(){var c=function(c){$(c).on("focus",function(){$(this).prev('input[type="radio"]').prop("checked",!0)})};return{checkOnFocus:c}}();ccEnchancement.checkOnFocus(".js-payment-amount-input");
+var ccEnchancement=function(){var n=function(n){$(n).on("focus",function(){$(this).siblings('input[type="radio"]').prop("checked",!0),$(this).attr("aria-required","true")}),$(n).on("blur",function(){$(this).attr("aria-required","false")})};return{checkOnFocus:n}}();ccEnchancement.checkOnFocus(".js-payment-amount-input");
